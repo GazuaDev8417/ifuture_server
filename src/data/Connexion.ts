114e20ac38f79dfa5ad683b89efba1704985327f
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2024-10-19 05:14:38 -0300

    ajustes

--- src/data/Connexion.ts
+++ src/data/Connexion.ts
@@ -7,7 +7,7 @@ config()
 export default abstract class ConnectToDatabase{
     protected static con = knex({
         client: 'pg',
-        connection: /* process.env.NEONTECH_DB */'postgresql://GazuaDev8417:oyxXs5u0rZim@ep-green-dew-a56t2dck.us-east-2.aws.neon.tech/tests?sslmode=require'
+        connection: process.env.NEONTECH_DB
     })
 
     public static testConnexion = async():Promise<void>=>{

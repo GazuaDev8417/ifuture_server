9aa8bfee27951d99db643ee26cc8bed2604315cc
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2024-11-12 18:23:57 -0300

    ajustes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import swaggerUi from 'swagger-ui-express'
 import * as SwaggerDocument from '../swagger.json'
 
 
+//console.log('Swagger document: ', SwaggerDocument)
+
 const PORT = process.env.PORT || 3003
 export const app = express()
 app.use(express.json())
@@ -21,3 +23,14 @@ app.listen(PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${PORT}`)
     console.log(`Documentação da API em http://localhost:${PORT}/api-docs`)
 })
+
+
+import fs from 'fs';
+
+try {
+    const fileContent = fs.readFileSync('swagger.json', 'utf-8');
+    console.log("Swagger file content loaded successfully:", fileContent);
+} catch (error) {
+    console.error("Error loading Swagger file:", error);
+}
+
